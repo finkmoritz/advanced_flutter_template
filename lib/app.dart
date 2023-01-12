663007ae464fcb976bfeb1514ce75403fe6603a3
Moritz Fink <moritz.fink@accenture.com>
2023-01-12 11:32:37 +0100

    Restructure widgets

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Advanced Flutter Template',
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
